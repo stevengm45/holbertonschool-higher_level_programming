22e226e1bd0d679063db2d3b1675b33dbd6fa5b3
stevengm45 <stevengm45@gmail.com>
2021-04-29 04:35:30 +0000

    pytho is the best

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+i = 0
+if __name__ == '__main__':
+    import sys
+    n = len(sys.argv)
+    if len(sys.argv) - 1 == 0:
+        print((len(sys.argv)-1), "arguments.")
+    elif len(sys.argv) - 1 == 1:
+        print((len(sys.argv)-1), "argument:")
+    elif len(sys.argv) > 1:
+        print((len(sys.argv)-1), "arguments:")
+    for i in range(1, n):
+        print(i, ":", sys.argv[i])
