773ddd4e28479faa44b555abf0fbebe250f90eba
stevengm45 <stevengm45@gmail.com>
2021-05-06 20:59:20 +0000

    python is fun

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,9 +2,9 @@
 
 
 def element_at(my_list, idx):
-    if __name__ == '__main__':
-        return element_at(0, 0)
-
     if (idx < 0 or idx > len(my_list)):
         return None
     return my_list[idx]
+
+if __name__ == '__main__':
+    element_at(0, 0)
