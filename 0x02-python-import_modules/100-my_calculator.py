a4c2c3a22616d79b364b935a8e478ce7ee98c582
stevengm45 <stevengm45@gmail.com>
2021-05-11 20:54:24 +0000

    python is the best

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,9 +7,17 @@ if __name__ == '__main__':
     a = int(sys.argv[1])
     operator = sys.argv[2]
     b = int(sys.argv[3])
-    if argv != 4:
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    if operator == '+':
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif operator == '-':
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif operator == '*':
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    elif operator == '/':
+        print("{} / {} = {}".format(a, b, div(a, b)))
+    else:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
-    elif
-    print("{} {} {} = {}".format(a, operator, b, r))
-            
