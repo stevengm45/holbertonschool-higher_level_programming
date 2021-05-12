0ab28d90c484ec9b1467c223095594a808261ca3
stevengm45 <stevengm45@gmail.com>
2021-05-12 19:49:31 +0000

    square * 2 in python

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = []
+    copy = []
     for i in matrix:
-        new_matrix.append(list(map(lambda x: x ** 2, i)))
-    return new_matrix
+        copy.append(list(map(lambda x: x ** 2, i)))
+    return copy
