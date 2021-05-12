d803a7d0390399bb461bca6754fa425b100f0c43
stevengm45 <stevengm45@gmail.com>
2021-05-12 00:28:08 +0000

    python holbies

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     copy = my_list.copy()
-    if idx > 0 and idx < (len(copy) - 1):
+    if idx < 0 and idx > (len(copy) - 1):
+        return copy
+    else:
         copy[idx] = element
     return copy
