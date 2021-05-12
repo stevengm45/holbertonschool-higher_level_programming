74e826978ba2b7824c867bff429a1c29bf61b353
stevengm45 <stevengm45@gmail.com>
2021-05-12 15:05:09 +0000

    programming

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence == ():
-        return 0, None
-    else:
-        return len(sentence), sentence[0]
+        sentence[0] = None
+        return sentence
+    return len(sentence), sentence[0]
