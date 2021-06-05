c163f8aaa1797c2f44835be268e863b67964a1f4
stevengm45 <stevengm45@gmail.com>
2021-06-05 03:53:10 +0000

    fix pep8 poitn 8 and 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Class Mylist that inherits from list"""
 
+
 class MyList(list):
     """Funcion to print sorted list"""
     def print_sorted(self):
--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,6 +2,7 @@
 """Implementing a new class called Rectangle """
 BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """Class Rectangle inheriting BaseGeometry class attributes"""
     def __init__(self, width, height):
