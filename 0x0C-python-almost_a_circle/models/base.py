8a1aef4097121d91022d3c4b552bba7aa9ea430e
stevengm45 <stevengm45@gmail.com>
2021-06-08 18:31:07 +0000

    algo

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,11 +4,13 @@ import json
 
 
 class Base:
+    """Class base"""
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """Inicialize the class"""
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
-            self.id = Base.__nb_objects
+            self.id = self.__nb_objects
