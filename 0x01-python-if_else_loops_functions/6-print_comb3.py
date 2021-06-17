ef5efdedc863c163832c446dad71ba165f8df51b
stevengm45 <stevengm45@gmail.com>
2021-06-17 02:30:22 +0000

    fix poitn six

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for num in range(0, 10):
-    for num2 in range(0, 10):
-        if num is 8 and num2 is 9:
-            print("{:d}{:d}".format(num, num2))
-        elif num < num2:
-            print("{:d}{:d}".format(num, num2), end=", ")
+for i in range(9):
+    for j in range(i + 1, 10):
+        if i * 10 + j < 89:
+            print("{:d}{:d}".format(i, j), end=", ")
+print("{:d}".format(89))
