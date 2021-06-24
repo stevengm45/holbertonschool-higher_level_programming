d82fa5e3d0c126115f028a1e1fee00b9f4c31879
usuario <usuario@usuario-pc>
2021-06-23 20:00:07 -0500

    fix in file 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+
+
 def new_in_list(my_list, idx, element):
     copy = my_list.copy()
-    if idx < 0 and idx > (len(copy) - 1):
-        return copy
-    else:
-        copy[idx] = element
+    if idx < 0 or idx >= (len(my_list)):
+        return my_list
+    tmp = my_list[:]
+    tmp[idx] = element
     return copy
