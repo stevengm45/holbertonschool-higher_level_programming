02fcb8e23ac22767486aaa32a08de9f414f6bdea
stevengm45 <stevengm45@gmail.com>
2021-07-13 20:15:23 -0500

    foreign key

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,6 @@
+-- creates the database hbtn_0d_usa and the table cities on your MySQL server
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbt_0d_usa.cities(id INT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	state_id INT NOT NULL FOREIGN KEY, name VARCHAR(256) NOT NULL,
+	FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id));
