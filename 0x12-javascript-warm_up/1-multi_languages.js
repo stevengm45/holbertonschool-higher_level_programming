d46407f16acc5e152ae39d79db176b7df23f2be4
stevengm45 <stevengm45@gmail.com>
2021-08-02 15:52:36 -0500

    programming is fun

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const varUno = 'C is fun';
+const varDos = 'Python is cool';
+const varTres = 'JavaScript is amazing';
+console.log(varUno);
+console.log(varDos);
+console.log(varTres);
