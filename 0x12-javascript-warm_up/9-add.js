8daa354c0a4db5e5492f2ea6b2542c616cdd9f3f
stevengm45 <stevengm45@gmail.com>
2021-08-02 18:26:53 -0500

    fix in algorimt javascript add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-const args = process.argv;
-const a = args[2];
-const b = args[3];
 function add(a, b) {
-  return (parseInt(a) + parseInt(b));
+  return a + b;
   }
-console.log(add(a, b));
+const args = process.argv;
+console.log(add(parseInt(args[2]), parseInt(args[3])));
