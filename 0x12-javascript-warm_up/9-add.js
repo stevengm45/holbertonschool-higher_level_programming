297031d859fef990a54e55816c273b6240c71654
stevengm45 <stevengm45@gmail.com>
2021-08-02 18:22:49 -0500

    function add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,11 +3,6 @@ const args = process.argv;
 const a = args[2];
 const b = args[3];
 function add(a, b) {
-  if (isNaN(a) || isNaN(b)) {
-    return (NaN);
-  } else {
-    let sum = parseInt(a) + parseInt(b);
-    return (sum);
+  return (parseInt(a) + parseInt(b));
   }
-}
 console.log(add(a, b));
