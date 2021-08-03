54acf85c489c0c20a8917b86cd5f2d5bcef3d827
stevengm45 <stevengm45@gmail.com>
2021-08-02 23:32:22 -0500

    fix in variable

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-const secMax = 0;
+let secMax = 0;
 const args = process.argv.slice(2);
 if (args.length > 1) {
   args.sort();
   secMax = args[args.length - 2];
 }
 console.log(secMax);
-
