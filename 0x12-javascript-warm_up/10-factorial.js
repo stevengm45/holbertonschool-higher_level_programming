3dedc6c81f2d369d1bee235e54f8bf14b9ac44d0
stevengm45 <stevengm45@gmail.com>
2021-08-02 23:15:33 -0500

    fix in style standar 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const args = process.argv[2];
+const args = process.argv[2]
 function factorial (args) {
   if (isNaN(args) || args === 1) {
-    return (1);
+    return (1)
   } else {
-    return (args * factorial(args -1));
+    return (args * factorial(args - 1))
   }
 }
-console.log(factorial(parseInt(args)));
+console.log(factorial(parseInt(args)))
