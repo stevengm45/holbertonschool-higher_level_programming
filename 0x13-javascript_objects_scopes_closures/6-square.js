45446d871c7588ac475dbd052d778f425c5c485b
stevengm45 <stevengm45@gmail.com>
2021-08-03 21:57:41 -0500

    square with x and c in js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const newSquare = require('./5-square.js');
+module.exports = class Square extends newSquare {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+        for (let i = 0; i < this.width; i++) {
+	  console.log(c.repeat(this.height));
+	}
+    }
+  }
+};
