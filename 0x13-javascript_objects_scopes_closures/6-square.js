de5bee17ece17c51609d0b1a3c4f228e459811a1
stevengm45 <stevengm45@gmail.com>
2021-08-03 22:03:38 -0500

    fix in style of file, error in spaces

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -6,8 +6,8 @@ module.exports = class Square extends newSquare {
       this.print();
     } else {
       for (let i = 0; i < this.width; i++) {
-	console.log(c.repeat(this.height));
-	}
+        console.log(c.repeat(this.height));
+      }
     }
   }
 };
