89ea8b576dec4c14bb3ccfcefab15d4054858a63
stevengm45 <stevengm45@gmail.com>
2021-08-03 21:58:51 -0500

    fix in style of file

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -5,8 +5,8 @@ module.exports = class Square extends newSquare {
     if (c === undefined) {
       this.print();
     } else {
-        for (let i = 0; i < this.width; i++) {
-	  console.log(c.repeat(this.height));
+      for (let i = 0; i < this.width; i++) {
+	console.log(c.repeat(this.height));
 	}
     }
   }
