2e38a321bf055347affb4af351618f0965391193
stevengm45 <stevengm45@gmail.com>
2021-08-20 22:27:48 -0500

    change in code add import and try and except

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 """ This script that takes your GitHub credentials (username and password)
     and uses the GitHub API to display your id"""
-if __name__ == '__main__':
-    import requests
-    from requests.auth import HTTPBasicAuth
-    from sys import argv
-    r = requests.get('https://api.github.com/users/{}'.format(argv[1]),
-                     auth=HTTPBasicAuth(argv[1], argv[2]))
-    print(r.json().get('id'))
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    response = requests.get("https://api.github.com/user",
+                            auth=requests.auth.HTTPBasicAuth(argv[1], argv[2]))
+    try:
+        text = response.json()
+        print(text.get('id'))
+    except:
+        print("Not a valid JSON")
