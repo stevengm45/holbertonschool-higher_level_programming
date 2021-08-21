c97bf62a7c8c208225d792d44473df5aa4092102
stevengm45 <stevengm45@gmail.com>
2021-08-20 21:52:28 -0500

    fix in space

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,5 +5,5 @@ import urllib.request
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
-    print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content:{}"
+    print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
           .format(type(html), html, html.decode("utf-8")))
