51541c9fdd3d55cb3a4b53f6370bd23e030bf81e
stevengm45 <stevengm45@gmail.com>
2021-09-14 21:11:46 -0500

    readme js

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const fs = require('fs');
+const argv = process.argv;
+
+fs.readFile(argv[2], 'utf8', (err, data) => {
+	if (err) {
+		return console.error(err);
+	}
+	console.log(data);
+});
+
